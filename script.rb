0d8e39374a00f280dd30ded177cf7257f18152fc
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-06-15 13:02:43 +0300

    Write the stock_picker method

--- script.rb
+++ script.rb
@@ -1,6 +1,10 @@
 prices = [17,3,6,9,15,8,6,1,10]
 
-days = prices.combination(2).max_by { |a,b| (b - a)}
-best_buy = days.first
-best_sell = days.last
-profit = [ prices.index(best_buy), prices.index(best_sell)]
+def stock_picker (array)
+  days = array.combination(2).max_by { |a,b| (b - a)}
+  best_buy = days.first
+  best_sell = days.last
+  profit = [ array.index(best_buy), array.index(best_sell)]
+end
+
+p stock_picker (prices)
