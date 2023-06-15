04d79a2f8cc555180721d4d5507230d0f880097d
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-06-15 12:57:46 +0300

    Put the best days to buy and sell in an array

--- script.rb
+++ script.rb
@@ -3,3 +3,4 @@ prices = [17,3,6,9,15,8,6,1,10]
 days = prices.combination(2).max_by { |a,b| (b - a)}
 best_buy = days.first
 best_sell = days.last
+profit = [ prices.index(best_buy), prices.index(best_sell)]
